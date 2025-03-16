658a35a88f61497ff298ade56564339fdf458e2b
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-03-16 15:54:55 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+
 import tailwindcss from "@tailwindcss/vite"
 import mdx from "@mdx-js/rollup"
 
